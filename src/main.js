32e743d5ef654fc7d7282904a38f932884b2aafb
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-09-19 20:00:02 +0200

    Build upon geowiki-viewer

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'leaflet'
+import 'geowiki-viewer/src/data'
+import 'geowiki-viewer/src/lang'
+import 'geowiki-viewer/src/map'
+import 'geowiki-viewer/src/layer'
+import 'geowiki-viewer/src/config'
+import App from 'geowiki-viewer/src/App'
+
+window.onload = function () {
+  return new App()
+}
