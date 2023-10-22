c7b7c96fc96570ff80d24a0351e0693d2a40ac8f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-10-22 13:38:20 +0200

    StyleSelector: add a select where used styles are selectable

---
+++ src/styleSelector.js
@@ -0,0 +1,35 @@
+import App from './App'
+App.addExtension({
+  id: 'stytleSelector',
+  initFun
+})
+
+function initFun (app, callback) {
+  const parentNode = document.getElementById('editor-style')
+
+  const dom = document.createElement('select')
+  parentNode.appendChild(dom)
+
+  app.on('state-apply', state => {
+    if (state.styleFile) {
+      const hasStyleFile = !!Array.from(dom.children)
+        .filter(option => option.value === state.styleFile)
+        .length
+
+      if (!hasStyleFile) {
+        const option = document.createElement('option')
+        option.value = state.styleFile
+        option.appendChild(document.createTextNode(state.styleFile))
+        dom.appendChild(option)
+      }
+
+      dom.value = state.styleFile
+    }
+  })
+
+  dom.onchange = () => {
+    app.stateApply({ styleFile: dom.value })
+  }
+
+  callback()
+}
